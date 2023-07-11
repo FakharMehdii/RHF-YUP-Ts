53e4078802f689b0c1b3e3cbcc14e8250417bbf5
Fakhar Mehdi <fakhar.mehdi@mergestack.com>
2023-07-11 15:32:04 +0500

    🚧 removed .env file

--- src/store/reducers/index.tsx
+++ src/store/reducers/index.tsx
@@ -1,4 +1,5 @@
-import { list } from "/home/mergestack/Desktop/Fakhar Training/React/Form + Yup to Ts/my-redux-hook-form-ts-app/src/store/reducers/listReducer";
+//import { list } from "/home/mergestack/Desktop/Fakhar Training/React/Form + Yup to Ts/my-redux-hook-form-ts-app/src/store/reducers/listReducer";
+import {list} from "src/store/reducers/listReducer"
 import { combineReducers } from "redux";
 
 export const rootReducer = combineReducers({
